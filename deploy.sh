e9286437c4fd5c82453f9cd172733a786d053e60
emiyayang <1038810929@qq.com>
2021-06-28 00:56:04 +0800

    chore: u

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,13 @@ set -e
 git branch -D gh-pages || echo ''
 git checkout -b gh-pages
 
-ls | grep -v public | grep -v node_modules | xargs rm -r
+ls | grep -v public | xargs rm -r
  
 cd public  
 cp -r . ..
 cd -
 rm -rf public
 
-rm -rf .gitignore
 rm -rf .babelrc
 
 git add .
