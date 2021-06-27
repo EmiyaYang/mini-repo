2aacc7869269e0bb204252b5afa40b506ee38369
emiyayang <1038810929@qq.com>
2021-06-28 00:14:48 +0800

    init

---
+++ REDAME.md
@@ -0,0 +1,1 @@
+https://github.com/kkocdko/kblog/blob/master/package.json
---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import path from "path";
+import { babel } from "@rollup/plugin-babel";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import serve from "rollup-plugin-serve";
+import pkg from "./package.json";
+
+const extensions = [".js", ".ts"];
+
+const resolve = function (...args) {
+  return path.resolve(__dirname, ...args);
+};
+
+module.exports = {
+  input: resolve("./src/index.ts"),
+  output: {
+    file: resolve("./public", pkg.main),
+    format: "esm",
+  },
+  plugins: [
+    serve({
+      port: 8080,
+      contentBase: "public",
+    }),
+    nodeResolve({
+      extensions,
+      modulesOnly: true,
+    }),
+    babel({
+      exclude: "node_modules/**",
+      extensions,
+    }),
+  ],
+};
