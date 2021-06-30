687066c4a412fecbbc282e12a9ef890ce1616985
emiyayang <1038810929@qq.com>
2021-06-30 20:51:00 +0800

    chore: add demo 4

---
+++ src/demo_3/index.ts
@@ -0,0 +1,36 @@
+document.addEventListener("copy", (e) => {
+  const selection = window.getSelection();
+
+  let selectedBlock;
+
+  if (selection.focusNode?.classList?.contains("block")) {
+    selectedBlock = selection.focusNode;
+  } else if (selection.anchorNode?.classList?.contains("block")) {
+    selectedBlock = selection.anchorNode;
+  } else {
+    selectedBlock =
+      selection.focusNode
+        // @ts-ignore
+        ?.querySelector?.(".block") ||
+      selection.anchorNode
+        // @ts-ignore
+        ?.querySelector?.(".block");
+  }
+
+  selectedBlock &&
+    e.clipboardData.setData("text/plain", selectedBlock.textContent);
+  selectedBlock &&
+    e.clipboardData.setData("text/html", selectedBlock.outerHTML);
+
+  e.preventDefault();
+
+  document.querySelector("#hint").innerHTML = "copied";
+});
+
+document.addEventListener("paste", (e) => {
+  console.log(e.clipboardData.items);
+
+  Array.from(e.clipboardData.items)?.forEach((item) =>
+    item.getAsString(console.log),
+  );
+});
